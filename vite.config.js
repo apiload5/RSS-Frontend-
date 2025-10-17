3d102102bc3d49b520623209ef19d8d60b51e412
apiload5 <amir03182055632@gmail.com>
2025-10-17 15:00:37 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/rss-generator-frontend/', // ← YEH CHANGE KARO
   build: {
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
   },
   server: {
     port: 3000,
