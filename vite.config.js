d9f9ab01326c5d2a256be8f13d3492d048dad003
apiload5 <amir03182055632@gmail.com>
2025-10-17 15:05:03 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rss-generator-frontend/', // ← YEH CHANGE KARO
+  base: '/rss-generator-frontend/', // GitHub repository name yahan dalo
   build: {
     outDir: 'dist',
     sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1600,
   },
   server: {
     port: 3000,
-    open: true
+    open: true,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
   }
 })
