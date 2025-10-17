f1c54926c0c30fc6f0144c60522c21d3bbd869c8
apiload5 <amir03182055632@gmail.com>
2025-10-17 22:26:56 +0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,6 @@
 export default {
   plugins: {
-    'tailwindcss/nesting': {}, // Enable nesting support
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-preset-env': {
-      stage: 1,
-      features: {
-        'focus-within-pseudo-class': false,
-        'nesting-rules': false, // Disable since we're using tailwindcss/nesting
-      },
-    },
-    'cssnano': process.env.NODE_ENV === 'production' ? {
-      preset: ['default', {
-        discardComments: {
-          removeAll: true,
-        },
-        normalizeWhitespace: true,
-        colormin: true,
-        minifyFontValues: true,
-        minifyGradients: true,
-        minifySelectors: true,
-      }]
-    } : false,
-  },
-        }
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
