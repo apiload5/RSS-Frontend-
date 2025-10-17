309d264845fe74e71d3213b8f94008bc2ab9b9eb
apiload5 <amir03182055632@gmail.com>
2025-10-17 23:23:11 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 👇️ Yeh line ZAROOR add karein
+  base: '/RSS-Frontend-/', 
   plugins: [react()],
-  base: '/RSS-Frontend-/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    emptyOutDir: true
-  }
 })
