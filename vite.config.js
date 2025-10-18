a2826eeb02c3b820c3dd7e843130795d6b3c53f5
apiload5 <amir03182055632@gmail.com>
2025-10-19 00:26:09 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 👇️ Yeh line ZAROOR add karein
+  // Yeh line GitHub Pages ke liye ZAROORI hai.
+  // Aapke repository ka naam "/RSS-Frontend-/" zaroor aana chahiye.
   base: '/RSS-Frontend-/', 
   plugins: [react()],
-})
+});
