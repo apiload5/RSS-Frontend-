60d104ab32897712328f36cfda86da277aea664a
apiload5 <amir03182055632@gmail.com>
2025-10-19 15:09:39 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // یہ تبدیل کریں
+  base: '',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
