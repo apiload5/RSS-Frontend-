84b1619deef282d49f49c1b7a02a538d2af92c96
apiload5 <amir03182055632@gmail.com>
2025-10-19 12:39:47 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBUp4eQaNFh1zZwt9hBJDssmc-Bdd2FjWk",
   authDomain: "aggregator-backend.firebaseapp.com",
   projectId: "aggregator-backend", 
-  storageBucket: "aggregator-backend.firebasestorage.app",
+  storageBucket: "aggregator-backend.firebasestorage.com",
   messagingSenderId: "920485477423",
   appId: "1:920485477423:web:36b0fb2df2d87c04f4dada"
 };
