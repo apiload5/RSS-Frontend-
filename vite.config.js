3f8254a9cc36f540190415fa53fd053a9411389e
apiload5 <amir03182055632@gmail.com>
2025-10-19 15:31:07 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RSS-Frontend/',  // آپ کا repository نام
+  base: './',  // موجودہ ڈائریکٹری
   build: {
     outDir: 'dist'
   }
