142d25c8ee100faff59a8ad8b659b5b97dd7928d
apiload5 <amir03182055632@gmail.com>
2025-10-19 20:40:55 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// آپ کی Firebase configuration
+// Use the CORRECT Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBUp4eQaNFh1zZwt9hBJDssmc-Bdd2FjWk",
   authDomain: "aggregator-backend.firebaseapp.com",
   projectId: "aggregator-backend",
-  storageBucket: "aggregator-backend.appspot.com",
-  messagingSenderId: "113222278115049707259",
-  appId: "1:1132222781150:web:d25206272ff7e44a30292b"
+  storageBucket: "aggregator-backend.firebasestorage.app",
+  messagingSenderId: "920485477423",
+  appId: "1:920485477423:web:36b0fb2df2d87c04f4dada",
+  measurementId: "G-7RWVKBKDS1"
 };
 
 // Initialize Firebase
@@ -16,6 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication
 export const auth = getAuth(app);
-
-// Firebase app export
 export default app;
